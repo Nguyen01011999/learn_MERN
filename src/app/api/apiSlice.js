dd5abf33e2d514793e2fc0b262284e195febe785
Nguyen01011999 <81405942+Nguyen01011999@users.noreply.github.com>
2023-09-07 19:12:27 +0700

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
     // baseUrl: 'http://localhost:3500',
-    baseUrl: 'https://learn-technotes-api.knn.com',
+    baseUrl: 'https://mern-nkn-api.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
