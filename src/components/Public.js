1c2e1dbf9d368f637091821d0d18cdfb9a1642d3
Nguyen01011999 <81405942+Nguyen01011999@users.noreply.github.com>
2023-09-08 11:16:41 +0700

    Update Public.js ...
    thay đổi

--- src/components/Public.js
+++ src/components/Public.js
@@ -4,10 +4,10 @@
 //     const content = (
 //         <section className="public">
 //             <header>
-//                 <h1>Welcome to <span className="nowrap">Dan D. Repairs!</span></h1>
+//                 <h1>Welcome to <span className="nowrap"> NKNnnnn... Repairs!</span></h1>
 //             </header>
 //             <main className="public__main">
-//                 <p>Located in Beautiful Downtown Foo City, Dan D. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
+//                 <p>Located in Beautiful Downtown Foo City, NKNnnnn... Repairs  provides a trained staff ready to meet your tech repair needs.</p>
 //                 <address className="public__addr">
 //                     Dan D. Repairs<br />
 //                     555 Foo Drive<br />
@@ -15,7 +15,7 @@
 //                     <a href="tel:+15555555555">(555) 555-5555</a>
 //                 </address>
 //                 <br />
-//                 <p>Owner: Dan Davidson</p>
+//                 <p>Owner: Nguyễn Khánh Nguyên</p>
 //             </main>
 //             <footer>
 //                 <Link to="/login">Employee Login</Link>
